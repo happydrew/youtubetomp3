7aed99df8e339c4430e120fca197f80db64ce560
zhugechina <zhugetd@126.com>
2025-04-20 01:42:17 +0800

    rebuild site.xml

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -2,8 +2,8 @@
 
 /** @type {import('next-sitemap').IConfig} */
 export default {
-  // siteUrl: "https://ezymp3.cc",
-  siteUrl: "https://youtubetomp3-dwj.pages.dev",
+  siteUrl: "https://ezymp3.cc",
+  // siteUrl: "https://youtubetomp3-dwj.pages.dev",
   generateIndexSitemap: false,
   generateRobotsTxt: false,
   // output: "export", // Set static output here
